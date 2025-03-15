df828255ae7b96351a2538888fc66e8ee4f046fb
joyjsmun <joy.js.mun@gmail.com>
2025-03-15 23:11:06 +0800

    ADD: updated route.ts imports

--- ai-joke-generator/app/api/chat/route.ts
+++ ai-joke-generator/app/api/chat/route.ts
@@ -1,16 +1,15 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
 
-const openai = new OpenAI();
+export const maxDuration = 30;
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    stream: true,
+  const result = streamText({
+    model: openai("gpt-4o-mini"),
     messages: [
       {
         role: "system",
@@ -20,6 +19,5 @@ export async function POST(req: Request) {
     ],
   });
 
-  const stream = OpenAIStream(response);
-  return new StreamingTextResponse(stream);
+  return result.toDataStreamResponse();
 }
